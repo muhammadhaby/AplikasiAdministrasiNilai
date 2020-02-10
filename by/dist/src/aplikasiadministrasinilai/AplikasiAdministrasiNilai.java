a05d26c5f9629ddce148f324b6b2868c236a3e1f
muhammadhaby <muhammadhaby59@gmail.com>
2020-02-10 17:35:03 +0700

    belajar pertama

---
+++ by/dist/src/aplikasiadministrasinilai/AplikasiAdministrasiNilai.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplikasiadministrasinilai;
+
+import view.FormUtama;
+
+/**
+ *
+ * @author haby
+ */
+public class AplikasiAdministrasiNilai {
+    private final static FormUtama forUtama = new FormUtama();
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        forUtama.setVisible(true);
+    }
+    
+}
---
+++ by/src/aplikasiadministrasinilai/AplikasiAdministrasiNilai.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplikasiadministrasinilai;
+
+import view.FormUtama;
+
+/**
+ *
+ * @author haby
+ */
+public class AplikasiAdministrasiNilai {
+    private final static FormUtama formUtama = new FormUtama();
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        formUtama.setVisible(true);
+    }
+    
+}
